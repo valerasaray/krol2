cf1613206f2f2d1f30ca788e34b9513bf5bf8dbe
crewch <86799725+crewch@users.noreply.github.com>
2022-06-02 23:16:49 +0300

    Update main.c

--- 11. Сортировка стека/main.c
+++ 11. Сортировка стека/main.c
@@ -6,23 +6,27 @@ int main(int argc, char const *argv[])
 {
     FILE *file = fopen(argv[1], "rb");
     int len = 0, c;
-    while(fscanf(file, "%d", &c) == 1) {
+    while(fscanf(file, "%d", &c)) {
         len++;
     }
+    
     fseek(file, 0, SEEK_SET);
     len = (int)sqrt(len);
     int matrix[len][len];
+    
     for (int i = 0; i < len; i++) {
         for (int j = 0; j < len; j++) {
-            fscanf(file, "%d", &matrix[i][j]);
+            fscanf(file, "%d", &matrix[j][i]);
         }
     }
+    
     for (int i = 0; i < len; i++) {
         for (int j = 0; j < len; j++) {
-            printf("%d ", matrix[j][i]);
+            printf("%d ", matrix[i][j]);
         }
         printf("\n");
     }
+    
     fclose(file);
     return 0;
 }
