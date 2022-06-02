7fa9d9707bc3e170a89f813995c7f0c0f3a908d3
pavlovid107b21 <90084802+pavlovid107b21@users.noreply.github.com>
2022-06-02 23:13:30 +0300

    Update main.c

--- 14. Удалить все листья у бинарного дерева на языке Си/main.c
+++ 14. Удалить все листья у бинарного дерева на языке Си/main.c
@@ -1,1 +1,19 @@
 
+node *delete(node *t)
+{
+	if (t == NULL) {
+		return;
+	}
+	if (t->left == NULL && t->right == NULL) {
+		free(t)
+		t = NULL;
+	} else if (t->left != NULL && t->right == NULL) {
+		t->left = delete(t->left);
+	} else if (t->left == NULL && t->right != NULL) {
+		t->right = delete(t->right);	
+	} else {
+		t->left = delete(t->left);
+		t->right = delete(t->right);
+	}
+	return t;
+}
