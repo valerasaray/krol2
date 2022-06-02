4ab50ba182050968aafc540248b188af2c2beb69
crewch <86799725+crewch@users.noreply.github.com>
2022-06-02 21:32:30 +0300

    Update main.c

--- 8. Найти глубину дерева/main.c
+++ 8. Найти глубину дерева/main.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+struct tree_node {
+    int value;
+    struct tree_node *left;
+    struct tree_node *right;
+};
+
+int depth(tree_node *root)
+{
+    if (root != NULL) {
+        return max(depth(root->left), depth(root->right)) + 1;
+    }
+    
+    return 0;
+}
