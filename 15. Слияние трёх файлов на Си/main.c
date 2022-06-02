da2ad27ffe9fb549d3972c7ba6bab097ed301f46
pavlovid107b21 <90084802+pavlovid107b21@users.noreply.github.com>
2022-06-02 23:26:58 +0300

    Update main.c

--- 15. Слияние трёх файлов на Си/main.c
+++ 15. Слияние трёх файлов на Си/main.c
@@ -1,1 +1,26 @@
 
+int main()
+{
+    FILE *file1, *file2, *file3, *output;
+
+  char c;
+
+  file1 = fopen(argv[1], "r");
+  file2 = fopen(argv[2], "r");
+  file3 = fopen(argv[3], "r");
+  output = fopen(argv[4], "w");
+
+  while ((c = fgetc(file1)) != EOF)
+    fputc(c, output);
+  while ((c = fgetc(file2)) != EOF)
+    fputc(c, output);
+  while ((c = fgetc(file3)) != EOF)
+    fputc(c, output);
+
+  fclose(file1);
+  fclose(file2);
+  fclose(file3);
+  fclose(output);
+
+  return 0;
+}
