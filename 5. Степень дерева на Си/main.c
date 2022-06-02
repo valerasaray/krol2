654194125dd9fde5d5b82f47a3b0d447b8a8bc21
crewch <86799725+crewch@users.noreply.github.com>
2022-06-02 21:06:18 +0300

    Update main.c

--- 5. Степень дерева на Си/main.c
+++ 5. Степень дерева на Си/main.c
@@ -1,1 +1,30 @@
+struct node {
+    char *name;
+    struct vector_i *children;
+};
 
+struct vector {
+    struct vector *prev;
+    struct vector *next;
+    struct node *man;
+};
+
+struct vector_i 
+    struct vector *prev;
+    struct vector *next;
+    int size;
+};
+
+int degree(struct vector *root)
+{
+    int max = root->man->children->size;
+    
+    while (root != NULL) {
+        if (max < root->man->children->size) {
+            max = root->man->children->size;
+        }
+        root->next;
+    }
+    
+    return max;
+}
