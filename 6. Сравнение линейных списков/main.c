6bc879c80ef60bc69a0e7c81e90bc6c7e00e8db2
crewch <86799725+crewch@users.noreply.github.com>
2022-06-02 21:02:54 +0300

    Update main.c

--- 6. Сравнение линейных списков/main.c
+++ 6. Сравнение линейных списков/main.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+struct list {
+    struct list *next;
+    struct list *prev;
+    int val;
+}
+
+int main()
+{
+    struct list *list1;
+    struct list *list2;
+    
+    int size1 = init_list(list1);
+    int size2 = init_list(list2);
+    
+    int flag = 1;
+    if (size1 == size2) {
+        while (list1 != NULL && list2 != NULL) {
+            if (list1->val != list2->val) {
+                flag = 0;
+            }
+        }
+    }
+    
+    if (flag) {
+        printf("OK");
+    } else {
+        printf("NO OK");
+    }
+    
+    return 0;
+}
