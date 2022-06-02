b82ad8e7416c65181db5d3c9a4d139871db73383
crewch <86799725+crewch@users.noreply.github.com>
2022-06-02 23:25:25 +0300

    Update main.c

--- 13. Подсчет количества разных элементов дерева/main.c
+++ 13. Подсчет количества разных элементов дерева/main.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
 
+struct tree_iter {
+    struct tree_node *node;
+    struct tree_iter *next;
+    struct tree_iter *prev;
+};
+
+struct decue {
+    struct decue *next;
+    struct decue *prev;
+    int value;
+};
+
+int main()
+{   
+    int cnt = 0;
+    struct decue *dc;
+    int size_d = init(dc);
+    struct tree_iter *iter;
+    struct decue *mem;
+    init(mem);
+    
+    while (iter != NULL) {
+        push_b(iter->node, dc);
+        iter = iter->next;
+    }
+    
+    while (dc != NULL) {
+        if (!in_dc(dc, mem)) {
+            push_b(dc, mem);
+            cnt++;
+        }
+        dc = dc->next;
+    }
+    
+    printf("%d\n", cnt);
+
+    return 0;
+}
