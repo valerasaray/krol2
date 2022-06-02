1517111589cc31b45241844da4ea9c09cb5d7066
pavlovid107b21 <90084802+pavlovid107b21@users.noreply.github.com>
2022-06-02 23:20:09 +0300

    Add files via upload

---
+++ 19. Счёт выражения в обратной польской записи/postfix_polska.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	stack<int> s;
+	char c;
+	int inp, a, b;
+	while (c != EOF) {
+		c = getchar();
+		if (isdigit(c)) {
+			ungetc(c, stdin);
+        	scanf("%d", &inp);
+        	s.push(inp);	
+		} else if (c == '+') {
+			a = s.top();
+			s.pop();
+			b = s.top();
+			s.pop();
+			s.push(a + b);
+		} else if (c == '-') {
+			a = s.top();
+			s.pop();
+			b = s.top();
+			s.pop();
+			s.push(a - b);
+		} else if (c == '*') {
+			a = s.top();
+			s.pop();
+			b = s.top();
+			s.pop();
+			s.push(a * b);
+		} else if (c == '/') {
+			a = s.top();
+			s.pop();
+			b = s.top();
+			s.pop();
+			s.push(a / b);
+		}
+	}
+    printf("\n%d\n", s.top());
+	return 0;
+}
