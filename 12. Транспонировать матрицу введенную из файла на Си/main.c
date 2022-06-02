e970117d3d70b12ff9e6e4939bdcdb5c618b224b
crewch <86799725+crewch@users.noreply.github.com>
2022-06-02 23:20:14 +0300

    Update main.c

--- 12. Транспонировать матрицу введенную из файла на Си/main.c
+++ 12. Транспонировать матрицу введенную из файла на Си/main.c
@@ -9,18 +9,22 @@ int main(int argc, char const *argv[])
 	while(fscanf(file, "%d", &c)) {
 		len++;
 	}
+	
 	fseek(file, 0, SEEK_SET);
 	len = (int)sqrt(len);
 	int matrix[len][len];
+	
 	for (int i = 0; i < len; i++) {
 		for (int j = 0; j < len; j++) {
 			fscanf(file, "%d", &matrix[j][i]);
 		}
 	}
+	
 	for (int i = 0; i < len; i++) {
 		for (int j = 0; j < len; j++) {
 			printf("%d ", matrix[i][j]);
 		}
 		printf("\n");
 	}
+	
 	return 0;
