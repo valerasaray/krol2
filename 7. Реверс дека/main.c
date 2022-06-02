7150bc30b1e688feac6d8ab6f84ec3bec28252e4
crewch <86799725+crewch@users.noreply.github.com>
2022-06-02 21:20:25 +0300

    Update main.c

--- 7. Реверс дека/main.c
+++ 7. Реверс дека/main.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+struct decue {
+    int *data;
+    int size;
+};
+
+void re(struct decue *dc)
+{
+	if (!is_empty(dc)) {
+		int tmp = pop_front(dc);
+		re(dc);
+		push_back(dc, tmp);
+	}
+}
+
+int main()
+{
+    struct decue *dc;
+    init_decue(dc);
+    
+    re(dc);
+    
+    return 0;
+}
