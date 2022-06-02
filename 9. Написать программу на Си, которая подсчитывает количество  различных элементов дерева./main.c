649d2f5fd3883c84a561359c5da56809b35f30a2
crewch <86799725+crewch@users.noreply.github.com>
2022-06-02 21:51:04 +0300

    Update main.c

--- 9. Написать программу на Си, которая подсчитывает количество  различных элементов дерева./main.c
+++ 9. Написать программу на Си, которая подсчитывает количество  различных элементов дерева./main.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
 
+struct tree_iter {
+    int value;
+    struct tree_node *next;
+    struct tree_node *prev;
+};
+
+struct decue {
+    struct decue *next;
+    struct decue *prev;
+    int value;
+};
+
+int main()
+{   
+    int cnt = 0;
+    struct decue *dc;
+    struct tree_iter *iter;
+    int size_d = init(dc);
+     struct decue *mem;
+     
+    while (iter != NULL) {
+        push_b(iter, dc);
+        iter = iter->next;
+    }
+    
+    while (iter != NULL) {
+        if (!in_dc(iter, mem)) {
+            push_b(iter, mem);
+            cnt++;
+        }
+        iter = iter->next;
+    }
+
+    printf("%d\n", cnt);
+
+    return 0;
+}
