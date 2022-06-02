b8abd78384d76ce2dbfd553f25e8e7c668ec12d6
pavlovid107b21 <90084802+pavlovid107b21@users.noreply.github.com>
2022-06-02 23:19:07 +0300

    Update main.c

--- 11. Сортировка стека/main.c
+++ 11. Сортировка стека/main.c
@@ -1,1 +1,38 @@
 
+int remove_max(stack∗ s)
+{
+	stack tmp;
+	create (&tmp);
+	int max = top(s);
+	while(!is_empty(s)) {
+		if (push(&tmp, pop(s))) {
+			if (top(&tmp) > max) {
+				max = top(&tmp);
+			}
+		}
+	}
+	int n = 0;
+	while (!is_empty(&tmp)) {
+		if((top(&tmp) == max) && (n == 0)) {
+			n++;
+			pop(&tmp);
+		}
+		else {
+			push(s, pop(&tmp));
+		}
+	}
+	destroy (&tmp);
+	return max;
+}
+
+void sort(stack ∗s)
+{
+	stack res;
+	init(&res);
+	while(!is_empty(s)){
+		push(&res, remove_max(s));
+	}
+	destroy(s);
+	∗s = res;
+	destroy (&res);
+}
