3c0c68aa04a8ed08bd8f0872d42b2719c5e1d2b4
pavlovid107b21 <90084802+pavlovid107b21@users.noreply.github.com>
2022-06-02 23:14:53 +0300

    Add files via upload

---
+++ 18. Проверка правильности вложенности скобок на Си/skobki.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	stack<char> s;
+	int top;
+	string c;
+	cin >> c;
+	for (int i = 0; i < c.size() - 1; i++) {
+		if (c[i] == '(') {
+			s.push(c[i]);
+		} else {
+			if (s.empty()) {
+				printf("FALSE\n");
+				return 0;
+			}
+			char top = s.top();
+			s.pop();
+			if (c[i] == '(' && top != ')') {
+				printf("FALSE\n");
+				return 0;
+			}	
+		}
+	}
+	s.pop();
+	if (s.empty()) {
+		printf("TRUE\n");
+	} else {
+		printf("FALSE\n");
+	}
+	return 0;
+}
