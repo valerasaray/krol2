2d1358a5f7c763f0f48c5c82c79f0f964db16b23
pavlovid107b21 <90084802+pavlovid107b21@users.noreply.github.com>
2022-06-03 00:06:14 +0300

    Add files via upload

---
+++ 17. Вычислить константное булевое выражение в постфиксной записи/const_bool_stack.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+int main(int argc, char const *argv[])
+{
+	stack<int> s;
+	char c;
+	int a, b;
+	while (c != EOF) {
+		c = getchar();
+		if (c == '0' || c == '1') {
+			s.push(c - '0');
+		} else if (c == '+') {
+			a = (int)(s.top());
+			s.pop();
+			b = (int)(s.top());
+			s.pop();
+			if (a && b) {
+				s.push(a);
+			} else {
+				s.push(a + b);
+			}
+		} else if (c == '*') {
+			a = (int)(s.top());
+			s.pop();
+			b = (int)(s.top());
+			s.pop();
+			s.push(a * b);
+		}
+	}
+	printf("\n%d\n", s.top());
+	return 0;
+}
