b2921f803c4b40d3cd372cf6dee692fa3c0143c7
pavlovid107b21 <90084802+pavlovid107b21@users.noreply.github.com>
2022-06-02 23:16:00 +0300

    Add files via upload

---
+++ 12. Транспонировать матрицу введенную из файла на Си/matrix_zzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+	
+int main(int argc, char const *argv[])
+{
+	FILE *file = fopen(argv[1], "rb");
+	int len = 0, c;
+	while(fscanf(file, "%d", &c) == 1) {
+		len++;
+	}
+	fseek(file, 0, SEEK_SET);
+	len = (int)sqrt(len);
+	int matrix[len][len];
+	for (int i = 0; i < len; i++) {
+		for (int j = 0; j < len; j++) {
+			fscanf(file, "%d", &matrix[i][j]);
+		}
+	}
+	for (int i = 0; i < len; i++) {
+		for (int j = 0; j < len; j++) {
+			printf("%d ", matrix[j][i]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
