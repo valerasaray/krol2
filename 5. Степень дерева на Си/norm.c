d831075100758e40b7bcfc5f233c064b17f9b342
Ilya Lyutoev <47085905+Uzym@users.noreply.github.com>
2022-06-03 18:02:10 +0300

    Update norm.c

--- 5. Степень дерева на Си/norm.c
+++ 5. Степень дерева на Си/norm.c
@@ -1,1 +1,34 @@
 
+typedef struct _tree {
+    int data;
+    struct _tree *child;
+    struct _tree *sibling;
+} Tree;
+
+int node_degree(Tree *t) {
+    if (t == NULL) {
+        return 0;
+    }
+    if (t->child == NULL) {
+        return 0;
+    }
+    Tree *cur = t->child;
+    int cnt = 1;
+    while (cur->sibling != NULL) {
+        cnt ++;
+        cur = cur->sibling;
+    }
+    return cnt;
+}
+
+int tree_degree(Tree *t) {
+    int deg = node_degree(t);
+    int sibDeg = tree_degree(t->sibling);
+    int chlDeg = tree_degree(t->child);
+    
+    if (deg > sibDeg && deg > chlDeg)
+        return deg;
+    if (sibDeg > chlDeg)
+        return sibDeg;
+    return chlDeg;
+}
