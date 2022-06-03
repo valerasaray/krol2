f7cb3cc0fd1e43d968a7476f0052d0e9d6c74628
Ilya Lyutoev <47085905+Uzym@users.noreply.github.com>
2022-06-03 18:17:33 +0300

    Update norm.c

--- 6. Сравнение линейных списков/norm.c
+++ 6. Сравнение линейных списков/norm.c
@@ -1,1 +1,31 @@
+typedef struct _list_node {
+    int data;
+    struct _list_node *next;
+} list_node;
 
+typedef struct _list {
+    list_node *head;
+    int size;
+} list;
+
+int check(list *l, list *r) {
+    list_node *a = l->head;
+    list_node *b = r->head;
+    int cur = 0;
+    while (cur < l->size && cur < r->size) {
+        if (a->data < b->data) {
+            return -1;
+        }
+        if (a->data > b->data) {
+            return 1;
+        }
+    }
+    if (l->size < r->size) {
+        return -1;
+    }
+    if (l->size > r->size) {
+        return 1;
+    }
+    
+    return 0;
+}
