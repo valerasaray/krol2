d79f7572d2edbe84e9ba4114cec249e045e38de3
Ilya Lyutoev <47085905+Uzym@users.noreply.github.com>
2022-06-03 17:52:14 +0300

    Update norm.c

--- 14. Удалить все листья у бинарного дерева на языке Си/norm.c
+++ 14. Удалить все листья у бинарного дерева на языке Си/norm.c
@@ -1,3 +1,21 @@
-void delete() {
-  
+void delete(tree *t) {
+    if (t == NULL) {
+        return;
+    }
+    if (t->left != NULL) {
+        if (t->left->left == NULL && t->left->right == NULL) {
+            free(t->left);
+            t->left = NULL;
+        } else {
+            delete(t->left);
+        }
+    }
+    if (t->right != NULL) {
+        if (t->right->left == NULL && t->right->right == NULL) {
+            free(t->right);
+            t->right = NULL;
+        } else {
+            delete(t->right);
+        }
+    }
 }
