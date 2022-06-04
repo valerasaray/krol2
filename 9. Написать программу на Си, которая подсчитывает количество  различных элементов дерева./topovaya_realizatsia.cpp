31a8eeebf4b33987079056228752c79923a0fb7f
Pavlov <90084802+pavlovid107b21@users.noreply.github.com>
2022-06-04 23:11:04 +0300

    сделал массив динамическим

--- 9. Написать программу на Си, которая подсчитывает количество  различных элементов дерева./topovaya_realizatsia.cpp
+++ 9. Написать программу на Си, которая подсчитывает количество  различных элементов дерева./topovaya_realizatsia.cpp
@@ -11,19 +11,19 @@ typedef struct node
 
 int bfs_count(tree *t)
 {
-	int arr[1000];
-	int arr_n = 0, flag = 1;
+	vector<int> v;
+	int flag = 1;
 	queue<tree *> q;
 	q.push(t);
 	tree *tmp = t;
 	while (!q.empty()) {
-		for (int i = 0; i < arr_n; i++) {
-			if (q.front()->data == arr[i]) {
+		for (int i = 0; i < v.size(); i++) {
+			if (q.front()->data == v[i]) {
 				flag = 0;
 			}
 		}
 		if (flag) {
-			arr[arr_n++] = q.front()->data;
+			v.push_back(q.front()->data);
 		}
 		flag = 1;
 		q.pop();
@@ -35,14 +35,14 @@ int bfs_count(tree *t)
 		}
 		tmp = q.front();
 	}
-	return arr_n;
+	return v.size();
 }
 
 
 int main(int argc, char const *argv[])
 {
 	tree *t = (tree *) malloc(sizeof(int));
-	t->data = 20;
+	t->data = 21;
 	t->right = (tree *) malloc(sizeof(int));
 	t->left = (tree *) malloc(sizeof(int));
 	t->right->data = 20;
