f68faf1c66ed437b6969f592a9206127c2198c9b
Ilya Lyutoev <47085905+Uzym@users.noreply.github.com>
2022-06-05 14:33:25 +0300

    Create chetcihk.c

---
+++ 18. Проверка правильности вложенности скобок на Си/chetcihk.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	char c;
+	int cnt = 0;
+	while (c != EOF) {
+		c = getchar();
+		if (c == '(')
+			cnt ++;
+		if (c == ')')
+			cnt --;
+		if (cnt < 0) {
+			printf("\nfalse\n");
+			return 0;
+		}
+	}
+	if (cnt != 0) {
+		printf("\nfalse\n");
+		return 0;
+	}
+	printf("\ntrue\n");
+	return 0;
+}
