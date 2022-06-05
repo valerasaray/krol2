2712c601e443824cc577cffde7dcaeb8224be017
Ilya Lyutoev <47085905+Uzym@users.noreply.github.com>
2022-06-05 14:43:12 +0300

    Create na_bst.c

---
+++ 9. Написать программу на Си, которая подсчитывает количество  различных элементов дерева./na_bst.c
@@ -0,0 +1,29 @@
+// Сложность алгоритма: O(n + m), где n - кол-во вершин обычного дерева, а m - кол-во ребер
+int bst_len(bst *b) {
+	if (b == NULL) {
+		return 0;
+	}
+	int l = bst_len(b->left);
+	int r = bst_len(b->right);
+
+	return l + r + 1;
+}
+
+void tree_dfs_bst(tree *t, bst *b) {
+	if (t == NULL) {
+		return;
+	}
+	bst_add(b, t->data);
+
+	tree_dfs_bst(t->child, b);
+	tree_dfs_bst(t->sibling, b);
+}
+
+int tree_count(tree *t) {
+	bst *b = bst_create();
+
+	tree_dfs_bst(t, b);
+
+	int cnt = bst_len(b);
+	return cnt;
+}
