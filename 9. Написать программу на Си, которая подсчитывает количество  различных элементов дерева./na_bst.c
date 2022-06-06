6d2a5644fae95cb587710a6c66ccc430d4630076
crewch <86799725+crewch@users.noreply.github.com>
2022-06-06 21:30:40 +0300

    Update na_bst.c

--- 9. Написать программу на Си, которая подсчитывает количество  различных элементов дерева./na_bst.c
+++ 9. Написать программу на Си, которая подсчитывает количество  различных элементов дерева./na_bst.c
@@ -1,12 +1,25 @@
 // Сложность алгоритма: O(n + m), где n - кол-во вершин обычного дерева, а m - кол-во ребер
-int bst_len(bst *b) {
+int bst_len(bst *b)
+{
 	if (b == NULL) {
 		return 0;
 	}
+	
 	int l = bst_len(b->left);
 	int r = bst_len(b->right);
 
 	return l + r + 1;
+}
+
+int tree_len(tree *t)
+{
+	if (t == NULL) {
+		return 0;
+	}
+	int chi = tree_len(t->child);
+	int sib = tree_len(t->sibling);
+
+	return chi + sib + 1;
 }
 
 void tree_dfs_bst(tree *t, bst *b) {
@@ -24,6 +37,6 @@ int tree_count(tree *t) {
 
 	tree_dfs_bst(t, b);
 
-	int cnt = bst_len(b);
+	int cnt = tree_len(t) - bst_len(b);
 	return cnt;
 }
